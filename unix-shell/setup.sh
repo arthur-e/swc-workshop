f484935dc994dda162f6d85a92f20c399ec86bd8
K. Arthur Endsley <endsley@umich.edu>
2015-12-29 11:54:23 -0500

    Modified setup script for Unix shell lesson

--- unix-shell/setup.sh
+++ unix-shell/setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get install tmux
-mkdir ~/workshop && cd ~/workshop && wget http://swcarpentry.github.io/shell-novice/shell-novice-data.zip
-unzip ~/shell-novice-data.zip -d ~/workshop
+mkdir ~/Desktop/shell-novice
+wget -O ~/Desktop/shell-novice/shell-novice-data.zip http://swcarpentry.github.io/shell-novice/shell-novice-data.zip
+unzip ~/Desktop/shell-novice/shell-novice-data.zip -d ~/Desktop/shell-novice
