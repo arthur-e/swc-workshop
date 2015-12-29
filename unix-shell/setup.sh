75e2967d9d01a2e4faf95a084067729be5030feb
K. Arthur Endsley <endsley@umich.edu>
2015-12-29 11:10:30 -0500

    Fixed bug in location of workshop folder for Unix shell lesson

--- unix-shell/setup.sh
+++ unix-shell/setup.sh
@@ -1,3 +1,3 @@
 sudo apt-get install tmux
-mkdir workshop && cd workshop && wget http://swcarpentry.github.io/shell-novice/shell-novice-data.zip
-unzip shell-novice-data.zip
+mkdir ~/workshop && cd ~/workshop && wget http://swcarpentry.github.io/shell-novice/shell-novice-data.zip
+unzip ~/shell-novice-data.zip -d ~/workshop
